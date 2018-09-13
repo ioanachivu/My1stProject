b61e9a5f39f31c92a63002b0bf45211c8b6745f7
ioanachivu <chivu.ioanaa@yahoo.com>
2018-09-13 12:32:25 +0300

    Battleships game

--- 1stProject/src/shipsGame/Game.java
+++ 1stProject/src/shipsGame/Game.java
@@ -8,16 +8,43 @@ public class Game {
 	private int noOfGuesses = 0;
 	
 	private void setUpGame() {
+		Ship one = new Ship();
+		Ship two = new Ship();
+		Ship three = new Ship();
 		
+		one.setName("Bombarder");
+		two.setName("Canoe");
+		three.setName("Viking");
+		
+		System.out.println("Your goal is to sink the three boats:");
+		System.out.println("Bombarder " + "Canoe "+ "Viking");
+		System.out.println("Try to sink them all in the fewest number of guesses");
+		
+		for(Ship s: shipsList) {
+			ArrayList<String> newLocation = helper.placeShips(3);
+			s.setLocationCells(newLocation);
+		}
 	}
 	
 	private void startPlaying() {
-		
+		while (!shipsList.isEmpty()) {
+			String userGuess = helper.getUserInput ("Enter a guess");
+			checkUserGuess(userGuess);
+		}
+		finishGame();
 	}
 	
 	private void checkUserGuess (String userGuess) {
 		
 	}
 	
+	private void finishGame() {
+		
+	}
 	
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.setUpGame();
+		game.startPlaying();
+	}
 }
--- 1stProject/src/shipsGame/Helper.java
+++ 1stProject/src/shipsGame/Helper.java
@@ -1,5 +1,17 @@
 package shipsGame;
 
+import java.util.ArrayList;
+
 public class Helper {
+
+	public ArrayList<String> placeShips(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getUserInput(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
