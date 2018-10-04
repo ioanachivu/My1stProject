9e8f7ee1c3954bf5a84161aead7a5ee628ae3fc5
ioanachivu <chivu.ioanaa@yahoo.com>
2018-10-04 15:06:31 +0300

    Add ActionLitener to button

--- 1stProject/src/gui/MainFrame.java
+++ 1stProject/src/gui/MainFrame.java
@@ -1,5 +1,8 @@
 package gui;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
@@ -13,7 +16,15 @@ public class MainFrame extends JFrame {
 		setLayout(new BorderLayout());
 		
 		add(textArea, BorderLayout.CENTER);
+		
 		add(button, BorderLayout.SOUTH);
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				textArea.append("Hello\n");
+				
+			}
+		});
 		
 		setSize(400,500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
