2e35a6ae22855e9e73eccbd3433865ec6017f3c1
ioanachivu <chivu.ioanaa@yahoo.com>
2018-11-01 16:40:08 +0200

    added image obj

--- 1stProject/graphicUI/Drawing.java
+++ 1stProject/graphicUI/Drawing.java
@@ -1,23 +1,26 @@
 package graphicUI;
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.*;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class Drawing extends JFrame {
 
 	@Override
 	public void paint(Graphics g) {
-		g.drawRect(50, 50, 50, 50);
+		g.drawRect(300, 300, 50, 50);
 		
 		g.setColor(Color.CYAN);
-		g.fillOval(100, 100, 50, 50);
+		g.fillOval(350, 350, 50, 50);
 		
 		g.setColor(Color.ORANGE);
-		g.fillRect(150, 150, 50, 80);
+		g.fillRect(400, 400, 50, 80);
+		
+		Image img = new ImageIcon("D://My Pictures//diferite//apus.jpg").getImage();
+		g.drawImage(img,0,0,400,250,this);
 	}
 	
 	public Drawing() {
-		setSize(300, 300);
+		setSize(600, 600);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
