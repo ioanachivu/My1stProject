db10ae366ca6f4aad7ec547cf9e81ded8ee15fe9
ioanachivu <chivu.ioanaa@yahoo.com>
2018-11-08 12:41:13 +0200

    DB connection

---
+++ 1stProject/DBDemo/Demo.java
@@ -0,0 +1,19 @@
+package DBDemo;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+class Demo {
+	public static void main(String args[]) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn = null;
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "");
+			System.out.print("Database is connected !");
+			conn.close();
+		} catch (Exception e) {
+			System.out.print("Do not connect to DB - Error:" + e);
+		}
+	}
+}
