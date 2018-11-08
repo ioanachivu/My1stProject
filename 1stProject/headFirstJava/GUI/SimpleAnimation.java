706f8aa027e62107949d4e3d4a603af58609c6b8
ioanachivu <chivu.ioanaa@yahoo.com>
2018-11-08 17:05:05 +0200

    simple code

---
+++ 1stProject/headFirstJava/GUI/SimpleAnimation.java
@@ -0,0 +1,44 @@
+package headFirstJava.GUI;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SimpleAnimation {
+
+	int x = 70;
+	int y = 70;
+	
+	public SimpleAnimation () {
+		JFrame frame = new JFrame();
+		MyDrawPanel drawPanel = new MyDrawPanel();
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(drawPanel);
+		frame.setSize(300, 300);	
+		frame.setVisible(true);
+		
+		for (int i=0; i<=130; i++) {
+			x++; y++;
+			drawPanel.repaint();
+			
+			try {
+				Thread.sleep(50);
+			} catch (Exception e) {}
+		}
+	}
+	
+	public static void main(String[] args) {
+		new SimpleAnimation();
+	}
+	
+	// inner class
+	class MyDrawPanel extends JPanel {
+		public void paintComponent(Graphics g) {
+			g.setColor(Color.pink);
+			g.fillOval(x, y, 50, 50);
+		}
+	}
+}
