2f04c7b20a0987e64834e3feb3d21f18a886a413
ioanachivu <chivu.ioanaa@yahoo.com>
2018-11-26 15:22:49 +0200

    updated the maps examples

--- 1stProject/javaCollections/Maps.java
+++ 1stProject/javaCollections/Maps.java
@@ -13,22 +13,28 @@ public class Maps {
 		Map<Integer, String> linkedHashMap = new LinkedHashMap<Integer, String>();
 		// keeps the values in the natural order
 		Map<Integer, String> treeMap = new TreeMap<Integer, String>();
-		
-		testMap(hashMap); System.out.println();
-		testMap(linkedHashMap); System.out.println();
+
+		testMap(hashMap);
+		System.out.println();
+		testMap(linkedHashMap);
+		System.out.println();
 		testMap(treeMap);
 	}
 
-public static void testMap(Map<Integer, String> map) {
-	map.put(3, "banana");
-	map.put(5, "carrot");
-	map.put(4, "snake");
-	map.put(2, "fish");
-	map.put(8, "crocodile");
-	
-	for(Integer key: map.keySet()) {
-		String value = map.get(key);
-		System.out.println(key+": "+value);
+	public static void testMap(Map<Integer, String> map) {
+		map.put(3, "banana");
+		map.put(5, "carrot");
+		map.put(4, "snake");
+		map.put(2, "fish");
+		map.put(8, "crocodile");
+
+		for (Integer key : map.keySet()) {
+			String value = map.get(key);
+			System.out.println(key + ": " + value);
+		}
+
+		if (map.containsKey(2)) {
+			System.out.println(map.get(2));
 		}
 	}
 }
