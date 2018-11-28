f1f17ad7d4d11684aa35c4570d5b11573afa106c
ioanachivu <chivu.ioanaa@yahoo.com>
2018-11-28 16:17:21 +0200

    multi threading example

---
+++ 1stProject/headFirstJava/multiThreading/Example.java
@@ -0,0 +1,54 @@
+package headFirstJava.multiThreading;
+
+class BankAccount {
+	private int balance = 100;
+	
+	public int getBalance() {
+		return balance;
+	}
+	
+	public void withdraw (int amount) {
+		balance = balance - amount;
+	}
+}
+
+public class Example implements Runnable {
+
+	private BankAccount account = new BankAccount();
+	
+	public static void main(String[] args) {
+		Example theJob = new Example();
+		Thread one = new Thread(theJob);
+		Thread two = new Thread(theJob);
+		one.start();
+		two.start();
+	}
+	
+	public void run() {
+		for (int x = 0; x<10; x++) {
+			makeWithdrawal(10);
+			if(account.getBalance() <0) {
+				System.out.println("Overdrawn!");
+			}
+		}
+	}
+	
+	private void makeWithdrawal(int amount) {
+		if(account.getBalance() >= amount) {
+			System.out.println(Thread.currentThread().getName() + " is about to withdraw");
+			
+			try {
+				System.out.println(Thread.currentThread().getName() + " is going to sleep");
+				Thread.sleep(500);
+			} catch (InterruptedException ex) {
+				ex.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName() + " woke up");
+			account.withdraw(amount);
+			System.out.println(Thread.currentThread().getName() + " compleates the withdrawal");
+		}
+		else {
+			System.out.println("Sorry, not enaugh for "+ Thread.currentThread().getName());
+		}
+	}
+}
